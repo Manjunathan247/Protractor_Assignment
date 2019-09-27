4d8db2eb59e49f8efb93e612066569c958af748f
Manjunathan247 <bmanjuba247@gmail.com>
2019-09-27 18:43:19 +0530

    A

--- config_file.js
+++ config_file.js
@@ -2,6 +2,10 @@ exports.config =
     {
         seleniumAddress: 'http://localhost:4444/wd/hub',
 
+        capabilities: {
+            browserName: 'chrome',
+          },        
+
         framework: 'custom',
         frameworkPath: require.resolve('protractor-cucumber-framework'),
 
